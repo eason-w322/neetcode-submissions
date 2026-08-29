3e02c8f966a23ccc2c4a9300c83bab80f93a3db4
eason-w322 <ywei27@berkeley.edu>
2026-08-29 01:02:56 -0700

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        memo = {}
+        def solve(i):
+            if i >= n:
+                return 0
+            if i == n-1:
+                return nums[i]
+            
+            if i in memo:
+                return memo[i]
+            result = max(nums[i] + solve(i+2), solve(i+1))
+            memo[i] = result
+            return result
+        return solve(0)
