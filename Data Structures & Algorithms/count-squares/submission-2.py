4265f07058a076a8880476c106d5a67309d54d84
eason-w322 <ywei27@berkeley.edu>
2026-09-19 15:52:08 -0700

    Add: count-squares - submission-2

---
+++ Data Structures & Algorithms/count-squares/submission-2.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+class CountSquares:
+
+    def __init__(self):
+        self.points = defaultdict(int)
+        self.same_vertical = defaultdict(list)
+        
+
+    def add(self, point: List[int]) -> None:
+        x, y = point
+        self.points[(x,y)] += 1
+        self.same_vertical[x].append(y)
+
+    def count(self, point: List[int]) -> int:
+        qx, qy = point
+        count = 0
+        for py in self.same_vertical[qx]:
+            if py == qy:
+                continue
+            distance = qy - py
+            for d in (qx - abs(distance), qx + abs(distance)):
+                    count += self.points[(d, qy)]*self.points[(d, py)]
+        return count
+        
